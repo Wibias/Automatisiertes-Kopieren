b80143337b0be3c7b552b830b9d01e55dab28168
Wibias <37517432+Wibias@users.noreply.github.com>
2023-09-23 16:23:51 +0200

    Make ValidationHelper.cs better

--- ValidationHelper.cs
+++ ValidationHelper.cs
@@ -1,6 +1,7 @@
 ﻿using Serilog;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Windows;
 using static Automatisiertes_Kopieren.FileManager.StringUtilities;
@@ -9,21 +10,51 @@ namespace Automatisiertes_Kopieren
 {
     public static class ValidationHelper
     {
+        public static bool IsValidPath(string path)
+        {
+            if (!Directory.Exists(path))
+                return false;
+
+            try
+            {
+                Path.GetFullPath(path);
+            }
+            catch
+            {
+                return false;
+            }
+
+            string tempFile = Path.Combine(path, "tempFileToCheckWritePermission.txt");
+            try
+            {
+                using (FileStream fs = File.Create(tempFile, 1, FileOptions.DeleteOnClose))
+                {
+                    // Do nothing, just create the file and close it
+                }
+            }
+            catch
+            {
+                return false;
+            }
+
+            return true;
+        }
+
         public static (string directoryPath, string fileName)? DetermineProtokollbogen(double monthsAndDays)
         {
             Dictionary<double, (string directoryPath, string fileName)> protokollbogenMap = new Dictionary<double, (string, string)>
             {
-                { 10.15, ("\\Entwicklungsboegen\\Krippe-Protokollboegen", "Kind_Protokollbogen_12_Monate") },
-                { 16.15, ("\\Entwicklungsboegen\\Krippe-Protokollboegen", "Kind_Protokollbogen_16_Monate") },
-                { 22.15, ("\\Entwicklungsboegen\\Krippe-Protokollboegen", "Kind_Protokollbogen_24_Monate") },
-                { 27.15, ("\\Entwicklungsboegen\\Krippe-Protokollboegen", "Kind_Protokollbogen_30_Monate") },
-                { 33.15, ("\\Entwicklungsboegen\\Krippe-Protokollboegen", "Kind_Protokollbogen_36_Monate") },
-                { 39.15, ("\\Entwicklungsboegen\\Ele-Protokollboegen", "Kind_Protokollbogen_42_Monate") },
-                { 45.15, ("\\Entwicklungsboegen\\Ele-Protokollboegen", "Kind_Protokollbogen_48_Monate") },
-                { 51.15, ("\\Entwicklungsboegen\\Ele-Protokollboegen", "Kind_Protokollbogen_54_Monate") },
-                { 57.15, ("\\Entwicklungsboegen\\Ele-Protokollboegen", "Kind_Protokollbogen_60_Monate") },
-                { 63.15, ("\\Entwicklungsboegen\\Ele-Protokollboegen", "Kind_Protokollbogen_66_Monate") },
-                { 69.15, ("\\Entwicklungsboegen\\Ele-Protokollboegen", "Kind_Protokollbogen_72_Monate") },
+                { 10.15, (Path.Combine("Entwicklungsboegen", "Krippe-Protokollboegen"), "Kind_Protokollbogen_12_Monate") },
+                { 16.15, (Path.Combine("Entwicklungsboegen", "Krippe-Protokollboegen"), "Kind_Protokollbogen_16_Monate") },
+                { 22.15, (Path.Combine("Entwicklungsboegen", "Krippe-Protokollboegen"), "Kind_Protokollbogen_24_Monate") },
+                { 27.15, (Path.Combine("Entwicklungsboegen", "Krippe-Protokollboegen"), "Kind_Protokollbogen_30_Monate") },
+                { 33.15, (Path.Combine("Entwicklungsboegen", "Krippe-Protokollboegen"), "Kind_Protokollbogen_36_Monate") },
+                { 39.15, (Path.Combine("Entwicklungsboegen", "Ele-Protokollboegen"), "Kind_Protokollbogen_42_Monate") },
+                { 45.15, (Path.Combine("Entwicklungsboegen", "Ele-Protokollboegen"), "Kind_Protokollbogen_48_Monate") },
+                { 51.15, (Path.Combine("Entwicklungsboegen", "Ele-Protokollboegen"), "Kind_Protokollbogen_54_Monate") },
+                { 57.15, (Path.Combine("Entwicklungsboegen", "Ele-Protokollboegen"), "Kind_Protokollbogen_60_Monate") },
+                { 63.15, (Path.Combine("Entwicklungsboegen", "Ele-Protokollboegen"), "Kind_Protokollbogen_66_Monate") },
+                { 69.15, (Path.Combine("Entwicklungsboegen", "Ele-Protokollboegen"), "Kind_Protokollbogen_72_Monate") },
             };
 
             foreach (var entry in protokollbogenMap.OrderByDescending(kvp => kvp.Key))
@@ -40,7 +71,6 @@ namespace Automatisiertes_Kopieren
 
         public static string? ValidateKidName(string kidName, string homeFolder, string groupDropdownText)
         {
-            // Exit if no name is provided
             if (string.IsNullOrWhiteSpace(kidName))
             {
                 Log.Error("Kid name is empty or whitespace.");
@@ -48,23 +78,19 @@ namespace Automatisiertes_Kopieren
                 return null;
             }
 
-            string groupFolder = ConvertSpecialCharacters(groupDropdownText, ConversionType.Umlaute);  // Use the converted group name
+            string groupFolder = ConvertSpecialCharacters(groupDropdownText, ConversionType.Umlaute);
 
-            string groupPath = $@"{homeFolder}\Entwicklungsberichte\{groupFolder} Entwicklungsberichte\Aktuell";  // Use the passed in parameter
+            string groupPath = $@"{homeFolder}\Entwicklungsberichte\{groupFolder} Entwicklungsberichte\Aktuell";
 
-            // Log the constructed group path for debugging
-            Log.Information($"Constructed group path: {groupPath}");
-
-            if (!System.IO.Directory.Exists(groupPath))
+            if (!IsValidPath(groupPath))
             {
-                Log.Error($"Group path does not exist: {groupPath}");
+                Log.Error($"Der Gruppenpfad ist nicht gültig oder zugänglich: {groupPath}");
                 MessageBox.Show($"Der Pfad für den Gruppenordner {groupFolder} ist nicht zugänglich. Bitte überprüfen Sie den Pfad und versuchen Sie es erneut.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
                 return null;
             }
 
             bool kidNameExists = System.IO.Directory.GetDirectories(groupPath).Any(dir => dir.Split(System.IO.Path.DirectorySeparatorChar).Last().Equals(kidName, StringComparison.OrdinalIgnoreCase));
 
-            // Log the result of the kid name check
             Log.Information($"Kid name exists in group directory: {kidNameExists}");
 
             if (!kidNameExists)
@@ -81,14 +107,12 @@ namespace Automatisiertes_Kopieren
         {
             if (string.IsNullOrWhiteSpace(reportYearText))
             {
-                return null;  // Handle how you'd like to exit if no year is provided.
+                return null;
             }
 
-            bool isValidYear = int.TryParse(reportYearText, out int parsedYear) && parsedYear >= 2023 && parsedYear <= 2099;
-
-            if (!isValidYear)
+            if (!int.TryParse(reportYearText, out int parsedYear) || parsedYear < 2023 || parsedYear > 2099)
             {
-                throw new Exception("Das Jahr muss aus genau 4 Ziffern bestehen, und zwischen 2023 und 2099 liegen. Bitte geben Sie ein gültiges Jahr ein.");
+                throw new ArgumentException("Das Jahr muss aus genau 4 Ziffern bestehen, und zwischen 2023 und 2099 liegen. Bitte geben Sie ein gültiges Jahr ein.");
             }
 
             return parsedYear;
