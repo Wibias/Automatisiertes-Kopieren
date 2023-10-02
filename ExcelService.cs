6295325be20689b382541bba6e41ccab047ded0a
Wibias <37517432+Wibias@users.noreply.github.com>
2023-10-02 16:50:26 +0200

    Make stuff better

--- ExcelService.cs
+++ ExcelService.cs
@@ -5,147 +5,156 @@ using System.Globalization;
 using System.IO;
 using System.Windows;
 using System.Windows.Controls;
+using static System.DateTime;
 using static Automatisiertes_Kopieren.FileManager.StringUtilities;
 using static Automatisiertes_Kopieren.LoggingService;
 
-namespace Automatisiertes_Kopieren
+namespace Automatisiertes_Kopieren;
+
+public class ExcelService
 {
-    public class ExcelService
+    private readonly string _homeFolder;
+    private static readonly LoggingService LoggingService = new();
+
+    public ExcelService(string homeFolder)
     {
-        private readonly string _homeFolder;
-        private static readonly LoggingService LoggingService = new();
+        _homeFolder = homeFolder ?? throw new ArgumentNullException(nameof(homeFolder));
+    }
+    private string? ConvertedGroupName { get; set; }
+    private string? ShortGroupName { get; set; }
+    public (double? months, string? error, string? parsedBirthDate, string? gender) ExtractFromExcel(string group, string kidLastName, string kidFirstName)
+    {
+        ConvertedGroupName = ConvertSpecialCharacters(group, ConversionType.Umlaute);
+        ShortGroupName = ConvertedGroupName.Split(' ')[0];
+        var filePath = $@"{_homeFolder}\Entwicklungsberichte\{ConvertedGroupName} Entwicklungsberichte\Monatsrechner-Kinder-Zielsetzung-{ShortGroupName}.xlsm";
+        string? parsedBirthDate = null;
+        var genderValue = string.Empty;
+        double? extractedMonths = null;
 
-        public ExcelService(string homeFolder)
+        if (string.IsNullOrEmpty(_homeFolder))
         {
-            _homeFolder = homeFolder ?? throw new ArgumentNullException(nameof(homeFolder));
+            LoggingService.ShowMessage("Bitte setzen Sie zuerst den Heimordner.", MessageType.Error);
+            return (null, "HomeFolderNotSet", parsedBirthDate, genderValue);
         }
-        private string? ConvertedGroupName { get; set; }
-        private string? ShortGroupName { get; set; }
-        public (double? months, string? error, string? parsedBirthDate, string? gender) ExtractFromExcel(string group, string kidLastName, string kidFirstName)
+
+        try
         {
-            ConvertedGroupName = ConvertSpecialCharacters(group, ConversionType.Umlaute);
-            ShortGroupName = ConvertedGroupName.Split(' ')[0];
-            var filePath = $@"{_homeFolder}\Entwicklungsberichte\{ConvertedGroupName} Entwicklungsberichte\Monatsrechner-Kinder-Zielsetzung-{ShortGroupName}.xlsm";
-            string? parsedBirthDate = null;
-            var genderValue = string.Empty;
-            double? extractedMonths = null;
+            using var workbook = new XLWorkbook(filePath);
+            var mainWorksheet = workbook.Worksheet("Monatsrechner");
 
-            if (string.IsNullOrEmpty(_homeFolder))
+            for (var row = 7; row <= 31; row++)
             {
-                LoggingService.ShowMessage("Bitte setzen Sie zuerst den Heimordner.", MessageType.Error);
-                return (null, "HomeFolderNotSet", parsedBirthDate, genderValue);
-            }
+                var lastNameCell = mainWorksheet.Cell(row, 3).Value.ToString();
+                var firstNameCell = mainWorksheet.Cell(row, 4).Value.ToString();
 
-            try
-            {
-                using var workbook = new XLWorkbook(filePath);
-                var mainWorksheet = workbook.Worksheet("Monatsrechner");
-
-                for (var row = 7; row <= 31; row++)
+                if (lastNameCell != null)
                 {
-                    var lastNameCell = mainWorksheet.Cell(row, 3).Value.ToString();
-                    var firstNameCell = mainWorksheet.Cell(row, 4).Value.ToString();
-
                     lastNameCell = lastNameCell.Trim();
-                    firstNameCell = firstNameCell.Trim();
-
-                    if (!string.Equals(lastNameCell, kidLastName, StringComparison.OrdinalIgnoreCase) ||
-                        !string.Equals(firstNameCell, kidFirstName, StringComparison.OrdinalIgnoreCase)) continue;
-                    var birthDate = mainWorksheet.Cell(row, 5).Value.ToString();
-                    DateTime.TryParse(birthDate, out DateTime parsedDate);
-                    parsedBirthDate = parsedDate.ToString("dd.MM.yyyy");
-
-                    var monthsValueRaw = mainWorksheet.Cell(row, 6).Value.ToString();
-
-                    if (double.TryParse(monthsValueRaw.Replace(",", "."), NumberStyles.Any, CultureInfo.InvariantCulture, out double parsedValue))
+                    if (firstNameCell != null)
                     {
-                        extractedMonths = Math.Round(parsedValue, 2);
+                        firstNameCell = firstNameCell.Trim();
+
+                        if (!string.Equals(lastNameCell, kidLastName, StringComparison.OrdinalIgnoreCase) ||
+                            !string.Equals(firstNameCell, kidFirstName, StringComparison.OrdinalIgnoreCase))
+                            continue;
                     }
                 }
 
-                var genderWorksheet = workbook.Worksheet("NAMES-BIRTHDAYS-FILL-IN");
+                var birthDate = mainWorksheet.Cell(row, 5).Value.ToString();
+                var parseBirthDate = TryParse(birthDate, out var parsedDate);
+                parsedBirthDate = parsedDate.ToString("dd.MM.yyyy");
 
-                for (var row = 4; row <= 28; row++)
+                var monthsValueRaw = mainWorksheet.Cell(row, 6).Value.ToString();
+
+                if (monthsValueRaw != null && double.TryParse(monthsValueRaw.Replace(",", "."), NumberStyles.Any, CultureInfo.InvariantCulture, out double parsedValue))
                 {
-                    var lastNameCell = genderWorksheet.Cell(row, 3).Value.ToString();
-                    var firstNameCell = genderWorksheet.Cell(row, 4).Value.ToString();
+                    extractedMonths = Math.Round(parsedValue, 2);
+                }
+            }
 
+            var genderWorksheet = workbook.Worksheet("NAMES-BIRTHDAYS-FILL-IN");
+
+            for (var row = 4; row <= 28; row++)
+            {
+                var lastNameCell = genderWorksheet.Cell(row, 3).Value.ToString();
+                var firstNameCell = genderWorksheet.Cell(row, 4).Value.ToString();
+
+                if (lastNameCell != null)
+                {
                     lastNameCell = lastNameCell.Trim();
-                    firstNameCell = firstNameCell.Trim();
-
-                    if (string.Equals(lastNameCell, kidLastName, StringComparison.OrdinalIgnoreCase) &&
-                        string.Equals(firstNameCell, kidFirstName, StringComparison.OrdinalIgnoreCase))
+                    if (firstNameCell != null)
                     {
-                        genderValue = genderWorksheet.Cell(row, 8).Value.ToString();
-                        break;
+                        firstNameCell = firstNameCell.Trim();
+
+                        if (!string.Equals(lastNameCell, kidLastName, StringComparison.OrdinalIgnoreCase) ||
+                            !string.Equals(firstNameCell, kidFirstName, StringComparison.OrdinalIgnoreCase))
+                            continue;
                     }
                 }
-            }
-            catch (FileNotFoundException)
-            {
-                LoggingService.LogAndShowMessage($"Die Datei {filePath} wurde nicht gefunden.",
-                                                  "Die Datei wurde nicht gefunden. Bitte überprüfen Sie den Pfad.");
-                return (null, "FileNotFound", parsedBirthDate, genderValue);
-            }
-            catch (IOException ioEx) when (ioEx.Message.Contains("because it is being used by another process"))
-            {
-                LoggingService.LogAndShowMessage($"Die Datei {filePath} wird von einem anderen Prozess verwendet.",
-                                                  "Die Excel-Datei ist geöffnet. Bitte schließen Sie die Datei und versuchen Sie es erneut.");
-                return (null, "FileInUse", parsedBirthDate, genderValue);
-            }
-            catch (Exception ex)
-            {
-                LoggingService.LogAndShowMessage($"Beim Verarbeiten der Excel-Datei ist ein unerwarteter Fehler aufgetreten: {ex.Message}",
-                                                  "Ein unerwarteter Fehler ist aufgetreten. Bitte versuchen Sie es später erneut.");
-                return (null, "UnexpectedError", parsedBirthDate, genderValue);
-            }
 
-            if (!extractedMonths.HasValue)
-            {
-                LoggingService.LogAndShowMessage($"Es konnte kein gültiger Monatswert für {kidFirstName} {kidLastName} extrahiert werden.",
-                                                  "Es konnte kein gültiger Monatswert extrahiert werden. Bitte überprüfen Sie die Daten.");
-                return (null, "ExtractionError", parsedBirthDate, genderValue);
+                genderValue = genderWorksheet.Cell(row, 8).Value.ToString();
+                break;
             }
-
-            return (extractedMonths, null, parsedBirthDate, genderValue);
+        }
+        catch (FileNotFoundException)
+        {
+            LoggingService.LogAndShowMessage($"Die Datei {filePath} wurde nicht gefunden.",
+                "Die Datei wurde nicht gefunden. Bitte überprüfen Sie den Pfad.");
+            return (null, "FileNotFound", parsedBirthDate, genderValue);
+        }
+        catch (IOException ioEx) when (ioEx.Message.Contains("because it is being used by another process"))
+        {
+            LoggingService.LogAndShowMessage($"Die Datei {filePath} wird von einem anderen Prozess verwendet.",
+                "Die Excel-Datei ist geöffnet. Bitte schließen Sie die Datei und versuchen Sie es erneut.");
+            return (null, "FileInUse", parsedBirthDate, genderValue);
+        }
+        catch (Exception ex)
+        {
+            LoggingService.LogAndShowMessage($"Beim Verarbeiten der Excel-Datei ist ein unerwarteter Fehler aufgetreten: {ex.Message}",
+                "Ein unerwarteter Fehler ist aufgetreten. Bitte versuchen Sie es später erneut.");
+            return (null, "UnexpectedError", parsedBirthDate, genderValue);
         }
 
-        private static void UpdateDateInWorksheet(string filePath, string worksheetName, string cellAddress, DateTime date)
+        if (extractedMonths.HasValue) return (extractedMonths, null, parsedBirthDate, genderValue);
+        LoggingService.LogAndShowMessage($"Es konnte kein gültiger Monatswert für {kidFirstName} {kidLastName} extrahiert werden.",
+            "Es konnte kein gültiger Monatswert extrahiert werden. Bitte überprüfen Sie die Daten.");
+        return (null, "ExtractionError", parsedBirthDate, genderValue);
+
+    }
+
+    private static void UpdateDateInWorksheet(string filePath, string worksheetName, string cellAddress, DateTime date)
+    {
+        var fileInfo = new FileInfo(filePath);
+        using var package = new ExcelPackage(fileInfo);
+        var worksheet = package.Workbook.Worksheets[worksheetName];
+
+        if (worksheet == null)
         {
-            var fileInfo = new FileInfo(filePath);
-            using var package = new ExcelPackage(fileInfo);
-            var worksheet = package.Workbook.Worksheets[worksheetName];
-
-            if (worksheet == null)
-            {
-                throw new Exception($"The worksheet '{worksheetName}' was not found in the file {filePath}.");
-            }
-
-            worksheet.Cells[cellAddress].Value = date;
-            package.Save();
+            throw new Exception($"The worksheet '{worksheetName}' was not found in the file {filePath}.");
         }
-        public string GetExcelFilePath(string groupName)
-        {
-            string convertedGroupName = ConvertSpecialCharacters(groupName, ConversionType.Umlaute);
-            string shortGroupName = convertedGroupName.Split(' ')[0];
-            return $@"{_homeFolder}\Entwicklungsberichte\{convertedGroupName} Entwicklungsberichte\Monatsrechner-Kinder-Zielsetzung-{shortGroupName}.xlsm";
-        }
-        public void SelectHeutigesDatumEntwicklungsBericht(object sender, RoutedEventArgs e)
-        {
-            var checkBox = sender as CheckBox;
-            if (checkBox != null && checkBox.IsChecked == true)
-            {
-                string filePath = $@"{_homeFolder}\Entwicklungsberichte\{ConvertedGroupName} Entwicklungsberichte\Monatsrechner-Kinder-Zielsetzung-{ShortGroupName}.xlsm";
 
-                try
-                {
-                    UpdateDateInWorksheet(filePath, "Monatsrechner", "D2", DateTime.Today);
-                }
-                catch (Exception ex)
-                {
-                    LoggingService.LogAndShowMessage(ex.Message, "Error updating Excel file.");
-                }
-            }
+        worksheet.Cells[cellAddress].Value = date;
+        package.Save();
+    }
+    public string GetExcelFilePath(string groupName)
+    {
+        var convertedGroupName = ConvertSpecialCharacters(groupName, ConversionType.Umlaute);
+        var shortGroupName = convertedGroupName.Split(' ')[0];
+        return $@"{_homeFolder}\Entwicklungsberichte\{convertedGroupName} Entwicklungsberichte\Monatsrechner-Kinder-Zielsetzung-{shortGroupName}.xlsm";
+    }
+
+    public void SelectHeutigesDatumEntwicklungsBericht(object sender, RoutedEventArgs e)
+    {
+        if (sender is not CheckBox { IsChecked: true }) return;
+        var filePath = $@"{_homeFolder}\Entwicklungsberichte\{ConvertedGroupName} Entwicklungsberichte\Monatsrechner-Kinder-Zielsetzung-{ShortGroupName}.xlsm";
+
+        try
+        {
+            UpdateDateInWorksheet(filePath, "Monatsrechner", "D2", Today);
+        }
+        catch (Exception ex)
+        {
+            LoggingService.LogAndShowMessage(ex.Message, "Error updating Excel file.");
         }
     }
 }
